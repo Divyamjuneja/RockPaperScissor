82575d490233fc64d73f4c006347967186de3493
Divyamjuneja <junejadivyam@gmail.com>
2023-12-28 18:26:30 +0530

    game finalised

--- external.js
+++ external.js
@@ -8,28 +8,22 @@ const result3 = "lost";
 
 let counterPlayer=0;
 let counterComputer=0;
-
+  
 var  button1 = document.querySelector("#btn1");
 var  button2 = document.getElementById('btn2');
 var  button3 = document.getElementById('btn3');
 
+
 let computerChoice = undefined;
 let computerSelection = getComputerChoice();
 
-function getPlayerChoice(playerSelection){
-        return playerSelection;
-    }
+let tie=0;
+let counterRound=0;
+let maxROund =5;
 
-button1.addEventListener('click',() => {
-    console.log(round(getPlayerChoice("rock"),getComputerChoice()));
-});
-button2.addEventListener('click',() => {
-    console.log(round(getPlayerChoice("paper"),getComputerChoice()));
-
-});
-button3.addEventListener('click',() => {
-    console.log(round(getPlayerChoice("scissor"),getComputerChoice()));
-});
+button1.addEventListener('click',() => game(option1));
+button3.addEventListener('click',() => game(option2));
+button2.addEventListener('click',() => game(option3));
 
 
 function getComputerChoice(){
@@ -55,15 +49,12 @@ function getComputerChoice(){
 function round(playerSelection,computerSelection){
     if(playerSelection == option1){
         if(computerSelection == option1){
-            console.log(option1);
             return result2;
         }
         else if(computerSelection == option2){
-            console.log(option2);
             return result1;
         }
         else if(computerSelection == option3){
-            console.log(option3);
             return result3;
         }
     }
@@ -91,53 +82,41 @@ function round(playerSelection,computerSelection){
     }
 }
 
+function game(playerChoice){
+    let computerChoice = getComputerChoice();
+    let output= round(playerChoice,computerChoice);
+    
+    if(output === result1 || output ===result3){
+        if(output === result1){
+            counterPlayer++;
+        }
+        else{
+            counterComputer++;
+        }
+        counterRound++;
+    }
+    else{
+        tie++;
+    }
 
-// let result =    round(getPlayerChoice(),getComputerChoice());
-// console.log(result);
-// button1.addEventListener('click' , () => alert("hello") );
-// function game(){
-//     //Best of 5
-//     let i=1;
-//     while(i<=5){
-//         let playerChoice = getPlayerChoice();
-//         let computerChoice = getComputerChoice();
-//         let opRound = round(playerChoice,computerChoice);
+        updateScore();
 
-//         if( opRound == result2){ 
-//             console.log("------------------")
-//             console.log("player chose " + playerChoice);
-//             console.log("computer chose " + computerChoice);
-//             console.log("Tie, Enter the value again!")
-//             continue;
-//         }
-//         else{
-//             if (opRound == result1){
-//                 counterPlayer++;
-//                 console.log("------------------")
-//                 console.log("player chose " + playerChoice);
-//                 console.log("computer chose " + computerChoice);
-//                 console.log("Round " + i + " is won by player:" + counterPlayer);
-//             }
-//             else if(opRound == result3){
-//                 counterComputer++;
-//                 console.log("-------------------")
-//                 console.log("computer chose " + computerChoice);
-//                 console.log("player chose " + playerChoice);
-//                 console.log("Round " + i + " is won by computer:" + counterComputer);
-//             }
-//             i++;
-//         }
-//     }
-//     if(counterPlayer>counterComputer){
-//         console.log("player wins")
-//     }
-//     else{
-//         console.log("computer wins")
-//     }
-//     }
-
-// game();
+    if(counterRound === maxROund){
+        if(counterPlayer>counterComputer){
+            alert("Player won");
+            
+        }
+        else{
+            alert("Computer won");
+        }
+        location.reload();
+    }
+}
 
 function updateScore(){
-    
+    document.getElementById('input1').textContent = counterPlayer;
+    document.getElementById('input2').textContent = counterComputer;
+    document.getElementById('input3').textContent = tie;
+    document.getElementById('result').textContent = "Computer Chose:" + computerChoice;
+
 }
