b4e4ecba25dcfbf1ab6cadbf5aa39ada6c8667f8
Divyamjuneja <junejadivyam@gmail.com>
2023-12-28 12:55:57 +0530

    Built a ui using css and added functionality using javascript, Adding DOM methodology for score printing os left

--- external.js
+++ external.js
@@ -9,13 +9,28 @@ const result3 = "lost";
 let counterPlayer=0;
 let counterComputer=0;
 
+var  button1 = document.querySelector("#btn1");
+var  button2 = document.getElementById('btn2');
+var  button3 = document.getElementById('btn3');
+
 let computerChoice = undefined;
 let computerSelection = getComputerChoice();
 
-function getPlayerChoice(){
-    let playerChoice = window.prompt("Enter your choice").toLowerCase();
-    return playerChoice;
-}
+function getPlayerChoice(playerSelection){
+        return playerSelection;
+    }
+
+button1.addEventListener('click',() => {
+    console.log(round(getPlayerChoice("rock"),getComputerChoice()));
+});
+button2.addEventListener('click',() => {
+    console.log(round(getPlayerChoice("paper"),getComputerChoice()));
+
+});
+button3.addEventListener('click',() => {
+    console.log(round(getPlayerChoice("scissor"),getComputerChoice()));
+});
+
 
 function getComputerChoice(){
     let selection = Math.random();
@@ -40,12 +55,15 @@ function getComputerChoice(){
 function round(playerSelection,computerSelection){
     if(playerSelection == option1){
         if(computerSelection == option1){
+            console.log(option1);
             return result2;
         }
         else if(computerSelection == option2){
+            console.log(option2);
             return result1;
         }
         else if(computerSelection == option3){
+            console.log(option3);
             return result3;
         }
     }
@@ -73,54 +91,52 @@ function round(playerSelection,computerSelection){
     }
 }
 
-// function ifTie(){
-//     if(round(playerChoice,getComputerChoice()) == result2){    
-//         return round(playerChoice,getComputerChoice());
+
+// let result =    round(getPlayerChoice(),getComputerChoice());
+// console.log(result);
+// button1.addEventListener('click' , () => alert("hello") );
+// function game(){
+//     //Best of 5
+//     let i=1;
+//     while(i<=5){
+//         let playerChoice = getPlayerChoice();
+//         let computerChoice = getComputerChoice();
+//         let opRound = round(playerChoice,computerChoice);
+
+//         if( opRound == result2){ 
+//             console.log("------------------")
+//             console.log("player chose " + playerChoice);
+//             console.log("computer chose " + computerChoice);
+//             console.log("Tie, Enter the value again!")
+//             continue;
+//         }
+//         else{
+//             if (opRound == result1){
+//                 counterPlayer++;
+//                 console.log("------------------")
+//                 console.log("player chose " + playerChoice);
+//                 console.log("computer chose " + computerChoice);
+//                 console.log("Round " + i + " is won by player:" + counterPlayer);
+//             }
+//             else if(opRound == result3){
+//                 counterComputer++;
+//                 console.log("-------------------")
+//                 console.log("computer chose " + computerChoice);
+//                 console.log("player chose " + playerChoice);
+//                 console.log("Round " + i + " is won by computer:" + counterComputer);
+//             }
+//             i++;
+//         }
 //     }
-// }
+//     if(counterPlayer>counterComputer){
+//         console.log("player wins")
+//     }
+//     else{
+//         console.log("computer wins")
+//     }
+//     }
 
-function game(){
-    //Best of 5
-    let i=1;
-    while(i<=5){
-        let playerChoice = getPlayerChoice();
-        let computerChoice = getComputerChoice();
-        let opRound = round(playerChoice,computerChoice);
-
-        if( opRound == result2){ 
-            console.log("------------------")
-            console.log("player chose " + playerChoice);
-            console.log("computer chose " + computerChoice);
-            console.log("Tie, Enter the value again!")
-            continue;
-        }
-        else{
-            if (opRound == result1){
-                counterPlayer++;
-                console.log("------------------")
-                console.log("player chose " + playerChoice);
-                console.log("computer chose " + computerChoice);
-                console.log("Round " + i + " is won by player:" + counterPlayer);
-            }
-            else if(opRound == result3){
-                counterComputer++;
-                console.log("-------------------")
-                console.log("computer chose " + computerChoice);
-                console.log("player chose " + playerChoice);
-                console.log("Round " + i + " is won by computer:" + counterComputer);
-            }
-            i++;
-        }
-    }
-    if(counterPlayer>counterComputer){
-        console.log("player wins")
-    }
-    else{
-        console.log("computer wins")
-    }
-    }
-
-game();
+// game();
 
 function updateScore(){
     
